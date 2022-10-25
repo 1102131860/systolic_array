20e283d48283070bbb45c49769f219b1860ebc7e
Li, Nealson Meng-Chian <nli79@gatech.edu>
2022-10-24 22:42:31 -0400

    Create cordic_pkg.sv

---
+++ src/verilog/cordic_pkg.sv
@@ -0,0 +1,16 @@
+package cordic_pkg;
+
+localparam [5:0] CORDIC_DATA_WIDTH = 16;
+
+typedef enum bit {
+    ROTATION = 1'b0,
+    VECTOR   = 1'b1
+} cordic_func;
+
+typedef struct packed{
+    logic signed [CORDIC_DATA_WIDTH-1:0] x;
+    logic signed [CORDIC_DATA_WIDTH-1:0] y;
+    logic signed [CORDIC_DATA_WIDTH-1:0] z;
+} cordic_data;
+
+endpackage
