77016b08ceb3fd13723ec715fee5407b32a3f210
Li, Nealson Meng-Chian <nli79@gatech.edu>
2022-10-24 22:40:20 -0400

    Update cordic_top.sv

--- src/verilog/cordic_top.sv
+++ src/verilog/cordic_top.sv
@@ -1,1 +1,18 @@
+import cordic_pkg::*;
 
+module cordic_top
+    #(
+        parameter NUM_MICRO_ROTATION = 12
+    )
+    (
+    (
+        input  logic        clk_i,
+        input  logic        rst_i,
+        
+        input  logic        en_i,
+        input  cordic_func  func_i,
+        input  cordic_data  data_i,
+        
+        output logic        done_o,
+        output cordic_data  data_o
+    );
