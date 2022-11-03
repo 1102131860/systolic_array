602d02ff6d9c9edf3a48ff4c5246ced1852278ab
Li, Nealson Meng-Chian <nli79@gatech.edu>
2022-11-03 07:45:32 -0400

    Update cordic_pkg.sv

--- src/verilog/cordic_pkg.sv
+++ src/verilog/cordic_pkg.sv
@@ -1,6 +1,7 @@
 package cordic_pkg;
 
 localparam [5:0] CORDIC_DATA_WIDTH = 16;
+localparam [5:0] CORDIC_OUTPUT_DATA_WIDTH = 18;
 
 typedef enum bit {
     ROTATION = 1'b0,
@@ -11,6 +12,12 @@ typedef struct packed{
     logic signed [CORDIC_DATA_WIDTH-1:0] x;
     logic signed [CORDIC_DATA_WIDTH-1:0] y;
     logic signed [CORDIC_DATA_WIDTH-1:0] z;
-} cordic_data;
+} cordic_data;   
+    
+typedef struct packed{
+    logic signed [CORDIC_OUTPUT_DATA_WIDTH-1:0] x;
+    logic signed [CORDIC_OUTPUT_DATA_WIDTH-1:0] y;
+    logic signed [CORDIC_OUTPUT_DATA_WIDTH-1:0] z;
+} cordic_output_data;
 
 endpackage
