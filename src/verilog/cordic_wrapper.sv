034e27812a434e60044c1ba3909be7373e6461f2
Li, Nealson Meng-Chian <nli79@gatech.edu>
2022-11-16 00:17:40 -0500

    Update cordic_wrapper.sv

--- src/verilog/cordic_wrapper.sv
+++ src/verilog/cordic_wrapper.sv
@@ -4,7 +4,8 @@ module cordic_wrapper
 #(
    parameter   NUM_MICRO_ROTATION = 12,
    parameter   INPUT_DATA_WIDTH   = 49,
-   parameter   OUTPUT_DATA_WIDTH  = 54
+   parameter   OUTPUT_DATA_WIDTH  = 54,
+   parameter   DATA_WIDTH         = 56
 )
 (
    input  logic                           i_clk,
@@ -15,9 +16,9 @@ module cordic_wrapper
    input  logic [INPUT_DATA_WIDTH-1:0]    i_stop_code,
    
    input  logic                           i_vld,
-   input  logic [INPUT_DATA_WIDTH-1:0]    i_data,
+   input  logic [DATA_WIDTH-1:0]          i_data,  //[INPUT_DATA_WIDTH-1:0]
    output logic                           o_vld,
-   output logic [OUTPUT_DATA_WIDTH-1:0]   o_data,
+   output logic [DATA_WIDTH-1:0]          o_data,  //[OUTPUT_DATA_WIDTH-1:0] 
    
    output logic                           o_sample_clk
 );
@@ -29,12 +30,13 @@ module cordic_wrapper
    logic          driver_mode, monitor_mode;
    logic          stop_code_hit;
    logic          sync_rst;
+   logic          [OUTPUT_DATA_WIDTH-1:0]       pre_o_data;
    
    assign o_sample_clk   = i_clk;
    
    assign driver_mode    = i_mode[1];    // 0=external   1=internal lfsr
    assign monitor_mode   = i_mode[0];    // 0=directout  1=signature analyzer
-   
+  
    assign driver_bypass  = i_bypass[2];  // 0=no bypass  1=bypass
    assign cordic_bypass  = i_bypass[1];  // 0=no bypass  1=bypass
    assign monitor_bypass = i_bypass[0];  // 0=no bypass  1=bypass
@@ -51,8 +53,8 @@ module cordic_wrapper
 //-------------------------------------------------------------------------//
    
    assign driver_in.vld  = i_vld;
-   assign driver_in.func = cordic_func'(i_data[INPUT_DATA_WIDTH-1]);
-   assign driver_in.data = i_data[INPUT_DATA_WIDTH-2:0];
+   assign driver_in.func = cordic_func'(i_data[54]); //[54]
+   assign driver_in.data = {i_data[51:36],i_data[33:18],i_data[15:0]};
    
    pseudo_rand_num_gen
    #(
@@ -120,6 +122,7 @@ module cordic_wrapper
       .o_data     (monitor_out.data)
    );
 
-   assign o_vld  = monitor_bypass ? monitor_in.vld  : monitor_out.vld;
-   assign o_data = monitor_bypass ? monitor_in.data : monitor_out.data;
+   assign o_vld      = monitor_bypass ? monitor_in.vld  : monitor_out.vld;
+   assign pre_o_data = monitor_bypass ? monitor_in.data : monitor_out.data;
+   assign o_data     ={2'b00, pre_o_data}; 
 endmodule
