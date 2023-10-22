2004ed283380c7eebd2f88e13bd950827915a654
Botkin, Garrett Charles <gbotkin@gatech.edu>
2023-10-22 15:28:53 -0400

    Update cordic_top.sv

--- src/verilog/cordic_top.sv
+++ src/verilog/cordic_top.sv
@@ -5,14 +5,14 @@ module cordic_top_groupnum
         parameter NUM_MICRO_ROTATION = 12
     )
     (
-        input  logic        i_clk,
-        input  logic        i_rst,
+        input  logic           i_clk,
+        input  logic           i_rst,
         
-        input  logic        i_valid,
-        input  cordic_func  i_func,
-        input  cordic_data  i_data,
+        input  logic           i_valid,
+        input  cordic_func     i_func,
+        input  cordic_data_in  i_data,
         
-        output logic        o_valid,
-        output cordic_data  o_data
+        output logic           o_valid,
+        output cordic_data     o_data
     );
 endmodule    
